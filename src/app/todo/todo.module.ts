1d7d78a67bebf2f71a8f6d3412d7facc73c1cf7a
Nazar Vanivskyi <staticman999@gmail.com>
2021-01-22 16:56:48 +0200

    add Angular Material

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,10 +1,19 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from "@angular/material/input";
 import TodoComponent from "./components/todo/todo.component";
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+  ],
   declarations: [TodoComponent],
 
   exports: [TodoComponent]
